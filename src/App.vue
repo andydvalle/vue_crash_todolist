ff17ccc08bc95e6d2e71951e4d60cf85fc331448
github username <andydelvalle89@gmail.com>
2020-09-22 19:27:21 -0700

    add routers

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    Todos,
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
-    };
+    Header,
   },
 };
 </script>
@@ -42,9 +22,19 @@ export default {
   margin: 0;
   padding: 0;
 }
-
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
